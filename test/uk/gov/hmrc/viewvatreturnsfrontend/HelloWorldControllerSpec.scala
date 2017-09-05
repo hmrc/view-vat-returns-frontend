35b6935deea8f32274af079a8928e8a69a0c83bf
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-09-05 12:04:12 +0100

    Updated controller specs for injection and added in WhitelistFilterSpec

--- test/uk/gov/hmrc/viewvatreturnsfrontend/HelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/viewvatreturnsfrontend/HelloWorldControllerSpec.scala
@@ -1,11 +1,29 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package uk.gov.hmrc.viewvatreturnsfrontend.controllers
 
-import controllers.HelloWorld
+import config.AppConfig
+import controllers.{HelloWorld}
 import play.api.http.Status
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.http._
+import play.api.i18n.MessagesApi
+import play.api.inject.Injector
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.play.test.UnitSpec
@@ -14,17 +32,23 @@ import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
 class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication{
 
-  val fakeRequest = FakeRequest("GET", "/")
+  lazy val injector: Injector = fakeApplication.injector
+  lazy val messages: MessagesApi = injector.instanceOf[MessagesApi]
+  lazy val mockConfig: AppConfig = injector.instanceOf[AppConfig]
+
+  implicit val fakeRequest = FakeRequest("GET", "/")
+
+  lazy val target = new HelloWorld(mockConfig, messages)
 
 
   "GET /" should {
     "return 200" in {
-      val result = HelloWorld.helloWorld(fakeRequest)
+      val result = target.helloWorld(fakeRequest)
       status(result) shouldBe Status.OK
     }
 
     "return HTML" in {
-      val result = HelloWorld.helloWorld(fakeRequest)
+      val result = target.helloWorld(fakeRequest)
       contentType(result) shouldBe Some("text/html")
       charset(result) shouldBe Some("utf-8")
     }
