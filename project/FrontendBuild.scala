f19ca4f53676330c49b58d6efc3a3022cb061a77
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2017-09-19 08:21:45 +0100

    removed other dependencies and updated library versions

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -28,19 +28,17 @@ object FrontendBuild extends Build with MicroService {
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % "8.6.0",
     "uk.gov.hmrc" %% "play-partials" % "6.0.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.6.0",
     "uk.gov.hmrc" %% "play-whitelist-filter" % "2.0.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "2.4.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
+    "org.jsoup" % "jsoup" % "1.10.2" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
-    "org.mockito" % "mockito-core" % "2.8.47" % scope
+    "org.mockito" % "mockito-core" % "2.9.0" % scope
   )
 
 }
