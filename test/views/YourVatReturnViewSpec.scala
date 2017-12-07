886b09152dbaafd8f361efb9401a8d699a6ba781
lewis-hay <lewis.hay@digital.hmrc.gov.uk>
2017-12-07 15:32:47 +0000

    Updated 9-box return page to match prototype

--- test/views/YourVatReturnViewSpec.scala
+++ test/views/YourVatReturnViewSpec.scala
@@ -60,7 +60,7 @@ class YourVatReturnViewSpec extends ViewBaseSpec {
     }
 
     "have the correct page heading" in {
-      elementText(Selectors.pageHeading) shouldBe "Your VAT return"
+      elementText(Selectors.pageHeading).contains("Your VAT return") shouldBe true
     }
 
     "have the correct subheading" in {
