2a55c330895a89463f7dfbcafa709750dcd3519f
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2017-12-20 16:52:50 +0000

    Added model, form and view for feature switch

--- test/views/ViewBaseSpec.scala
+++ test/views/ViewBaseSpec.scala
@@ -24,6 +24,7 @@ import play.api.i18n.{Lang, Messages, MessagesApi}
 import play.api.inject.Injector
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
+import play.twirl.api.Html
 import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.collection.JavaConversions._
@@ -55,6 +56,6 @@ class ViewBaseSpec extends UnitSpec with GuiceOneAppPerSuite {
     attributes.map(attribute => (attribute.getKey, attribute.getValue)).toMap
   }
 
-  def formatHtml(markup: String): String = Jsoup.parseBodyFragment(s"\n$markup\n").toString.trim
+  def formatHtml(markup: Html): String = Jsoup.parseBodyFragment(s"\n$markup\n").toString.trim
 
 }
