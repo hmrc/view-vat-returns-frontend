d6fff87411b20308a271277b2ead25d7ff2e9f1d
William Lea <wjlea93@gmail.com>
2018-03-06 10:40:15 +0000

    Updated auth logic

--- app/controllers/AuthorisedController.scala
+++ app/controllers/AuthorisedController.scala
@@ -41,15 +41,14 @@ abstract class AuthorisedController extends FrontendController with I18nSupport 
       val predicate = if (appConfig.features.simpleAuth()) {
         EmptyPredicate
       } else {
-        (Enrolment(vatDecEnrolmentKey) and Enrolment(vatVarEnrolmentKey) and Enrolment(mtdVatEnrolmentKey))
+        ((Enrolment(vatDecEnrolmentKey) or Enrolment(vatVarEnrolmentKey)) and Enrolment(mtdVatEnrolmentKey))
           .or(Enrolment(mtdVatEnrolmentKey))
       }
 
       enrolmentsAuthService.authorised(predicate).retrieve(Retrievals.authorisedEnrolments) {
-        enrolments => {
+        enrolments =>
           val user = if (appConfig.features.simpleAuth()) User("123456789") else User(enrolments)
           block(request)(user)
-        }
       } recoverWith {
         case _: NoActiveSession => Future.successful(Unauthorized(views.html.errors.sessionTimeout()))
         case _: AuthorisationException => Future.successful(Forbidden(views.html.errors.unauthorised()))
