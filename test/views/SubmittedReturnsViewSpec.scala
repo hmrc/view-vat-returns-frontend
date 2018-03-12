320ffa68d747f08d5730d16bf1a78e16a23be36b
Lewis Hay <lewis.hay@qa.com>
2018-03-12 15:22:07 +0000

    Added unit tests for tab and submitted returns templates

--- test/views/SubmittedReturnsViewSpec.scala
+++ test/views/SubmittedReturnsViewSpec.scala
@@ -49,133 +49,138 @@ class SubmittedReturnsViewSpec extends ViewBaseSpec {
     val previousReturnsLink = "div > div > p > a"
   }
 
-  val returnYears = Seq(2018, 2017)
+  "Rendering the Submitted Returns page with multiple return years displayed" when {
 
-  "Rendering the VAT Returns page with no returns for the selected year of 2018" should {
+    val returnYears = Seq(2018, 2017)
 
-    lazy val view = views.html.returns.submittedReturns(VatReturnsViewModel(returnYears, 2018, Seq(), hasNonMtdVat = false))
-    lazy implicit val document: Document = Jsoup.parse(view.body)
+    "there are multiple returns for the selected year of 2018" should {
 
-    "have the correct document title" in {
-      document.title shouldBe "Submitted returns"
-    }
-
-    "have the correct page heading" in {
-      elementText(Selectors.pageHeading) shouldBe "Submitted returns"
-    }
-
-    "render breadcrumbs which" should {
-
-      "have the text 'Business tax account'" in {
-        elementText(Selectors.btaBreadcrumb) shouldBe "Business tax account"
-      }
-
-      "link to BTA" in {
-        element(Selectors.btaBreadcrumbLink).attr("href") shouldBe "bta-url"
-      }
-
-      "have the text 'VAT'" in {
-        elementText(Selectors.vatBreadcrumb) shouldBe "VAT"
-      }
-
-      "link to the VAT Details page" in {
-        element(Selectors.vatBreadcrumbLink).attr("href") shouldBe "vat-details-url"
-      }
-
-      "have the text 'Submitted returns'" in {
-        elementText(Selectors.submittedReturnsBreadcrumb) shouldBe "Submitted returns"
-      }
-    }
-
-    "have tabs for each return year" should {
-
-      "tab one" should {
-
-        "have the text '2018'" in {
-          elementText(Selectors.tabOne) should include("2018")
-        }
-
-        "contain visually hidden text" in {
-          elementText(Selectors.tabOneHiddenText) shouldBe "Currently viewing returns from 2018"
-        }
-      }
-
-      "tab two" should {
-
-        "have the text '2017'" in {
-          elementText(Selectors.tabTwo) should include("2017")
-        }
-
-        s"contain a link to ${controllers.routes.ReturnObligationsController.submittedReturns(2017).url}" in {
-          element(Selectors.tabTwo).select("a").attr("href") shouldBe controllers.routes.ReturnObligationsController.submittedReturns(2017).url
-        }
-
-        "contain visually hidden text" in {
-          elementText(Selectors.tabTwoHiddenText) shouldBe "View returns from 2017"
-        }
-      }
-    }
-
-    "have the correct return heading" in {
-      elementText(Selectors.returnsHeading) shouldBe "2018 returns"
-    }
-
-    "have the correct alternate content when no returns are found" in {
-      elementText(Selectors.noReturnsFound) shouldBe "You haven’t submitted any returns for 2018 yet. You must use accounting software to submit your returns."
-    }
-  }
-
-  "Rendering the VAT Returns page with multiple returns for the selected year of 2018" should {
-
-    lazy val exampleReturns: Seq[ReturnObligationsViewModel] =
-      Seq(
-        ReturnObligationsViewModel(
-          LocalDate.parse("2018-01-01"),
-          LocalDate.parse("2018-12-31"),
-          "#001"
-        ),
-        ReturnObligationsViewModel(
-          LocalDate.parse("2018-01-01"),
-          LocalDate.parse("2018-09-30"),
-          "#002"
+      lazy val exampleReturns: Seq[ReturnObligationsViewModel] =
+        Seq(
+          ReturnObligationsViewModel(
+            LocalDate.parse("2018-01-01"),
+            LocalDate.parse("2018-12-31"),
+            "#001"
+          ),
+          ReturnObligationsViewModel(
+            LocalDate.parse("2018-01-01"),
+            LocalDate.parse("2018-09-30"),
+            "#002"
+          )
         )
-      )
 
-    lazy val view = views.html.returns.submittedReturns(VatReturnsViewModel(returnYears, 2018, exampleReturns, hasNonMtdVat = false))
-    lazy implicit val document: Document = Jsoup.parse(view.body)
+      lazy val view = views.html.returns.submittedReturns(VatReturnsViewModel(returnYears, 2018, exampleReturns, hasNonMtdVat = false))
+      lazy implicit val document: Document = Jsoup.parse(view.body)
 
-    "have the correct return heading" in {
-      elementText(Selectors.returnsHeading) shouldBe "2018 returns"
-    }
-
-    "have the correct period text" in {
-      elementText(Selectors.period) shouldBe "For the period:"
-    }
-
-    "contain the first return which" should {
-
-      "contains the correct obligation period text" in {
-        elementText(Selectors.obligation(1)) shouldBe "1 January to 31 December 2018"
+      "have the correct return heading" in {
+        elementText(Selectors.returnsHeading) shouldBe "2018 returns"
       }
 
-      "contains the correct link to view a specific return" in {
-        element(Selectors.obligationLink(1)).attr("href") shouldBe controllers.routes.ReturnsController.vatReturn(2018, "#001").url
+      "have the correct period text" in {
+        elementText(Selectors.period) shouldBe "For the period:"
+      }
+
+      "contain the first return which" should {
+
+        "contains the correct obligation period text" in {
+          elementText(Selectors.obligation(1)) shouldBe "1 January to 31 December 2018"
+        }
+
+        "contains the correct link to view a specific return" in {
+          element(Selectors.obligationLink(1)).attr("href") shouldBe controllers.routes.ReturnsController.vatReturn(2018, "#001").url
+        }
+      }
+
+      "contain the second return which" should {
+
+        "contains the correct obligation period text" in {
+          elementText(Selectors.obligation(2)) shouldBe "1 January to 30 September 2018"
+        }
+
+        "contains the correct link to view a specific return" in {
+          element(Selectors.obligationLink(2)).attr("href") shouldBe controllers.routes.ReturnsController.vatReturn(2018, "#002").url
+        }
       }
     }
 
-    "contain the second return which" should {
+    "there are no returns for the selected year of 2018" should {
 
-      "contains the correct obligation period text" in {
-        elementText(Selectors.obligation(2)) shouldBe "1 January to 30 September 2018"
+      lazy val view = views.html.returns.submittedReturns(VatReturnsViewModel(returnYears, 2018, Seq(), hasNonMtdVat = false))
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct document title" in {
+        document.title shouldBe "Submitted returns"
       }
 
-      "contains the correct link to view a specific return" in {
-        element(Selectors.obligationLink(2)).attr("href") shouldBe controllers.routes.ReturnsController.vatReturn(2018, "#002").url
+      "have the correct page heading" in {
+        elementText(Selectors.pageHeading) shouldBe "Submitted returns"
+      }
+
+      "render breadcrumbs which" should {
+
+        "have the text 'Business tax account'" in {
+          elementText(Selectors.btaBreadcrumb) shouldBe "Business tax account"
+        }
+
+        "link to BTA" in {
+          element(Selectors.btaBreadcrumbLink).attr("href") shouldBe "bta-url"
+        }
+
+        "have the text 'VAT'" in {
+          elementText(Selectors.vatBreadcrumb) shouldBe "VAT"
+        }
+
+        "link to the VAT Details page" in {
+          element(Selectors.vatBreadcrumbLink).attr("href") shouldBe "vat-details-url"
+        }
+
+        "have the text 'Submitted returns'" in {
+          elementText(Selectors.submittedReturnsBreadcrumb) shouldBe "Submitted returns"
+        }
+      }
+
+      "have tabs for each return year" should {
+
+        "tab one" should {
+
+          "have the text '2018'" in {
+            elementText(Selectors.tabOne) should include("2018")
+          }
+
+          "contain visually hidden text" in {
+            elementText(Selectors.tabOneHiddenText) shouldBe "Currently viewing returns from 2018"
+          }
+        }
+
+        "tab two" should {
+
+          "have the text '2017'" in {
+            elementText(Selectors.tabTwo) should include("2017")
+          }
+
+          s"contain a link to ${controllers.routes.ReturnObligationsController.submittedReturns(2017).url}" in {
+            element(Selectors.tabTwo).select("a").attr("href") shouldBe
+              controllers.routes.ReturnObligationsController.submittedReturns(2017).url
+          }
+
+          "contain visually hidden text" in {
+            elementText(Selectors.tabTwoHiddenText) shouldBe "View returns from 2017"
+          }
+        }
+      }
+
+      "have the correct return heading" in {
+        elementText(Selectors.returnsHeading) shouldBe "2018 returns"
+      }
+
+      "have the correct alternate content when no returns are found" in {
+        elementText(Selectors.noReturnsFound) shouldBe
+          "You haven’t submitted any returns for 2018 yet. You must use accounting software to submit your returns."
       }
     }
   }
 
-  "Rendering the VAT Returns page with only one returned year" when {
+  "Rendering the VAT Returns page with only one return year displayed" when {
 
     lazy val exampleReturns: Seq[ReturnObligationsViewModel] =
       Seq(
