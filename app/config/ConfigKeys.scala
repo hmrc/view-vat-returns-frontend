3f7b8d7a689135fc8a07385df8af75e15a37c719
ja-green <jack.green@digital.hmrc.gov.uk>
2018-03-19 11:28:10 +0000

    Added nine box feature to ConfigKeys

--- app/config/ConfigKeys.scala
+++ app/config/ConfigKeys.scala
@@ -37,6 +37,7 @@ object ConfigKeys {
   val allowPaymentsFeature: String = "features.allowPayments.enabled"
   val staticDateEnabledFeature: String = "features.date-service.staticDate.enabled"
   val staticDateValue: String = "date-service.staticDate.value"
+  val nineBoxFeature: String = "features.nineBox.enabled"
 
   val businessTaxAccountBase: String = "business-tax-account.host"
   val businessTaxAccountUrl: String = "business-tax-account.homeUrl"
