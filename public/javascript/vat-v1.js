e00b10331b027a8ecd17d2a0e864f4f47b3640a4
Sam Raspin <14580184+sraspin@users.noreply.github.com>
2019-10-25 16:52:41 +0100

    [BTAT-7073] - print historic vat returns now working as expected

--- public/javascript/vat-v1.js
+++ public/javascript/vat-v1.js
@@ -2,3 +2,22 @@ $(document).ready(function() {
   //initialise <details> polyfill from frontend toolkit
   GOVUK.details.init();
 });
+
+var mediaQueryList = window.matchMedia('print');
+
+var currH1 = document.getElementsByTagName("h1").item(0);
+var newH1 = 'Your VAT Return<span class="heading-secondary">' + currH1.innerText.substr(18) + '</span>';
+
+var returnTotal = document.getElementsByClassName("heading-large").item(0);
+var currReturnTotal = returnTotal.outerHTML;
+
+mediaQueryList.addListener(function(mql) {
+  if(mql.matches) {
+    document.getElementsByTagName("h1").item(0).innerHTML = newH1;
+    returnTotal.remove();
+    document.getElementsByClassName("button").item(0).insertAdjacentHTML("afterend", currReturnTotal);
+    console.log('webkit equivalent of onbeforeprint');
+  } else {
+    location.reload();
+  }
+});
